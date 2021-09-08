2bc819bf28a62feb98724b74bee7d28aaa66a7d6
Felix Geisendörfer <felix@felixge.de>
2021-09-08 14:57:39 +0200

    More details

--- cmd/pprofstats/main.go
+++ cmd/pprofstats/main.go
@@ -4,9 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sort"
+	"strings"
 
 	"github.com/felixge/pprofutils/internal"
 	"github.com/google/pprof/profile"
+	"github.com/olekukonko/tablewriter"
 )
 
 func main() {
@@ -18,7 +21,8 @@ func main() {
 
 func run() error {
 	var (
-		versionF = flag.Bool("version", false, "Print version and exit.")
+		versionF    = flag.Bool("version", false, "Print version and exit.")
+		recursionsF = flag.Int("r", 0, "Print top N recursive locations.")
 	)
 	flag.Parse()
 	if *versionF {
@@ -29,11 +33,78 @@ func run() error {
 	if err != nil {
 		return err
 	}
+
+	recursionLocations := map[*profile.Location]int64{}
+	var recursions int64
+	for _, s := range inProf.Sample {
+		seen := map[uint64]struct{}{}
+		for _, l := range s.Location {
+			if _, ok := seen[l.Address]; ok {
+				recursions++
+				recursionLocations[l] += 1
+			} else {
+				seen[l.Address] = struct{}{}
+			}
+		}
+	}
+
 	fmt.Printf("Sample Types: %d\n", len(inProf.SampleType))
-	fmt.Printf("Samples: %d\n", len(inProf.Sample))
+	fmt.Printf("Samples: %d (%d locations doing %d recursions)\n", len(inProf.Sample), len(recursionLocations), recursions)
 	fmt.Printf("Locations: %d\n", len(inProf.Location))
 	fmt.Printf("Functions: %d\n", len(inProf.Function))
 	fmt.Printf("Mappings: %d\n", len(inProf.Mapping))
 	fmt.Printf("Comments: %d\n", len(inProf.Comments))
+
+	if *recursionsF > 0 {
+		fmt.Printf("\nTop %d Recursive Locations:\n\n", *recursionsF)
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"ID", "Count", "Method", "File", "Line"})
+		table.SetBorder(false)
+		table.SetCenterSeparator("")
+		table.SetColumnSeparator("")
+		table.SetRowSeparator("")
+		table.SetHeaderLine(false)
+
+		for _, l := range topLocations(recursionLocations, *recursionsF) {
+			line := l.Line[0]
+			fn := line.Function
+			table.Append([]string{
+				fmt.Sprintf("%d", l.ID),
+				fmt.Sprintf("%d", l.Count),
+				fn.Name,
+				fn.Filename,
+				fmt.Sprintf("%d", line.Line),
+			})
+		}
+		table.Render()
+	}
+
 	return nil
+}
+
+func printSample(s *profile.Sample) {
+	var out []string
+	for _, l := range s.Location {
+		for _, line := range l.Line {
+			out = append(out, line.Function.Name)
+		}
+	}
+	fmt.Printf("%s\n", strings.Join(out, ";"))
+}
+
+func topLocations(locations map[*profile.Location]int64, n int) []TopLocation {
+	var top []TopLocation
+	for l, v := range locations {
+		top = append(top, TopLocation{l, v})
+	}
+	sort.Slice(top, func(i, j int) bool {
+		return top[i].Count > top[j].Count
+	})
+	return top[0:n]
+}
+
+type TopLocation struct {
+	*profile.Location
+	Count int64
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/google/pprof v0.0.0-20210423192551-a2663126120b
 	github.com/matryer/is v1.4.0
+	github.com/olekukonko/tablewriter v0.0.5
 )
